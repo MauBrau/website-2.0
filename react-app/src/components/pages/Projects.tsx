8621058f5c89867eda0f97807206993f53733d59
Maude <maude.braunstein@gmail.com>
2026-02-04 19:13:20 -0500

    Added link to "Alpha" build of Bingo game to site, enabled projects tab

--- react-app/src/components/pages/Projects.tsx
+++ react-app/src/components/pages/Projects.tsx
@@ -1,17 +1,37 @@
+import { Typography } from "@mui/material";
 import { ProjectsText } from "../../interface/IText";
-import Page from "./Page";
+import Page, { Body } from "./Page";
+import { textFont } from "../helper/Style";
 
 export default function Projects() {
     const pageText: ProjectsText = require("../../data/text/projects.json");
     return ( 
-        <Page
-            className="card"
-            minWidth={600}
-            desktopTopMarginAmount={125}
-            mobileTopMarginAmount={125}
-            title={pageText.title}
-            body={pageText.body}
-        >
-        </Page>
+        <div>
+            <Page
+                className="card"
+                minWidth={600}
+                desktopTopMarginAmount={125}
+                mobileTopMarginAmount={125}
+                title={pageText.title}
+                body={pageText.body}
+            >
+                <div>
+                    <Body text={`On top of this, I just released an unofficial, in-development, re-creation of Habs Bingo! The page might take a few seconds to load on first visit (free hosting :) ), but feel free to play along during a game. You can find it below:`} />
+                    <Typography
+                        component="a"
+                        href={"https://habs-bingo-dev-9861c9e4e14c.herokuapp.com/"} 
+                        sx={{
+                            flexGrow: 1,
+                            mr: 2,
+                            fontFamily: textFont,
+                            textAlign: { xs: "center", sm: "left" },
+                            color: "primary.main",
+                        }}
+                    >
+                        Habs Bingo
+                    </Typography>
+                </div>
+            </Page>
+        </div>
     );
 }
