fcf2995aa2fad20268323d29af4cc403f2abea23
Maude <maude.braunstein@gmail.com>
2026-05-12 14:59:02 -0400

    #3: github actions demo

--- node-runtime/jest.config.js
+++ node-runtime/jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
     testEnvironment: 'node',
     roots: ['<rootDir>'],
     testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
+    collectCoverageFrom: [
+        '**/*.ts',
+        '!**/__tests__/**',
+        '!**/node_modules/**',
+        '!**/dist/**',
+        '!**/webpack*.ts',
+        '!**/jest.config.*',
+    ],
 };
